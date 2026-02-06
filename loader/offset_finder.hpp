5f8ec954d8f215e52b9d9b312ded43411b1466ed
Searchstars <searchstars@163.com>
2026-02-06 16:17:11 +0800

    Revert "b"
    This reverts commit da6f916f984b75fd77ea553c756fe4f09a144f70.

--- loader/offset_finder.hpp
+++ loader/offset_finder.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <iostream>
-#include <cstdint>
 
 struct OffsetFinder {
 	auto setDefaultOffsets() -> void;
 	auto determineOffsets() -> bool;
 
 	std::uint64_t offsetHelperSyscall_;
-	std::uint64_t offsetJitTranslate_;
+	std::uint64_t offsetHelperResolveAddr_;
 };
